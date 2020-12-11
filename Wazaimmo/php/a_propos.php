7df2a24f95b8ffdd34ae8c41a27e635fd80d9f80
etienneoceane <etienne.oceane@outlook.com>
2020-12-11 13:25:25 +0100

    commit final

--- Wazaimmo/php/a_propos.php
+++ Wazaimmo/php/a_propos.php
@@ -1,9 +1,38 @@
 <?php 
     include("header.php");
 ?>
+<div align="center">
+<section style="background-color: rgb(205, 209, 209)" class="col-6" align="center">
+        <aside class="aside_index"> 
+            
+                <h2>Accueil</h2>
+                <hr id="hr_1">
+           
 
-BONJOUR AU REVOIR VOILA CYAO TCHUS
+            <article class="Article_1"> 
+                <h2>L'entreprise</h2>
+                <p class="1.1"> Notre entreprise familiale met tout son savoir-faire à votre disposition dans le domaine de l'immobilier !</p>
+                <p class="1.2"> Créée il y a 70 ans, notre entreprise vend fleurs, arbustes, matériel à main et motorisés.</p>
+                <p class="1.3"> Implantés à Amiens, nous intervenons dans tout le département de la Somme : Albert, Doullens, Péronne, Abbeville, Corbie.</p>			
+            </article> 
+       
+            <article class="article_2"> 
+                <h2>Qualité</h2>
+                <p class="2.1"> Nous mettons à votre disposition un service personnalisé, avec 1 seul interlocuteur durant tout votre projet.</p>
+                <p class="2.2"> Vous serez séduit par notre expertise, nos compétences et notre sérieux.</p>
+            </article>
+        
+            <article class="article_3">
+                <h2>Devis gratuit</h2>
+                <p class="2.3"> Vous pouvez bien sûr contacter pour de plus amples informations ou pour une demande d’intervention. <br>
+                          Vous souhaitez un devis ? Nous vous le réalisons gratuitement.</p>
+             </article>
+                <hr id="hr_2">
+        </aside>
 
+    
+    </section>
+    </div>
 <?php
     include("footer.php");
     
